98a622e283711cd3e372d3ac1999b714ac0945f7
Sangam Kumar <sangamcse.dev@gmail.com>
2020-05-28 01:55:06 +0530

    Now we tell you your browser sucks in your native tongue.
    Signed-off-by: Sangam Kumar <sangamcse.dev@gmail.com>

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -15,7 +15,7 @@ info "Installing OHMYZSH"
 git clone https://github.com/ohmyzsh/ohmyzsh.git "$OHMYZSH_PATH"
 ZSH="$OHMYZSH_PATH" sh "$OHMYZSH_PATH/tools/install.sh"
 
-curl https://raw.githubusercontent.com/zeit/zeit.zsh-theme/master/zeit.zsh-theme -Lo "$OHMYZSH_PATH/custom/themes/zeit.zsh-theme"
+curl https://raw.githubusercontent.com/zeit/zsh-theme/master/vercel.zsh-theme -Lo "$OHMYZSH_PATH/custom/themes/zeit.zsh-theme"
 
 info "Configuraing zsh..."
 
